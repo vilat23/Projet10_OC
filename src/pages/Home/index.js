597023c503cd11e39e71dd72027ef812b26c9bf5
Phimanao Paul <142014758+vilat23@users.noreply.github.com>
2024-05-03 01:20:44 +0200

    Correction erreus sur le menu --> ajout des id pour les ancres

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -22,7 +22,9 @@ const Page = () => {
       <section className="SliderContainer">
         <Slider />
       </section>
-      <section className="ServicesContainer">
+
+      {/* ----On ajoute un id à la section pour que l'ancre des liens du menu fonctionne */}
+      <section className="ServicesContainer" id="nos-services">
         <h2 className="Title">Nos services</h2>
         <p>Nous organisons des événements sur mesure partout dans le monde</p>
         <div className="ListContainer">
@@ -51,11 +53,15 @@ const Page = () => {
           </ServiceCard>
         </div>
       </section>
-      <section className="EventsContainer">
+
+      {/* ----On ajoute un id à la section pour que l'ancre des liens du menu fonctionne */}
+      <section className="EventsContainer" id="nos-realisations">
         <h2 className="Title">Nos réalisations</h2>
         <EventList />
       </section>
-      <section className="PeoplesContainer">
+
+      {/* ----On ajoute un id à la section pour que l'ancre des liens du menu fonctionne */}
+      <section className="PeoplesContainer" id="notre-equipe">
         <h2 className="Title">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
