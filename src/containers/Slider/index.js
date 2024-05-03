42f41995b056c0810dc366e778625a62b40f43e1
Phimanao Paul <142014758+vilat23@users.noreply.github.com>
2024-05-03 12:38:56 +0200

    add comments

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,27 +7,33 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
+
+  // ---Classement des evenements par ordre Croissant (à corriger en décroissant)-----
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
+
+  // ---Fonction pour passer au suivant
   const nextCard = () => {
     setTimeout(
       () => setIndex(index < byDateDesc.length ? index + 1 : 0),
       5000
     );
   };
+
   useEffect(() => {
     nextCard();
   });
+
+
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
         <>
           <div
             key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
+            className={`SlideCard SlideCard--${index === idx ? "display" : "hide" // affiche la slide correspondante à l'index actuel ou sinon cache la slide
+              }`}
           >
             <img src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
@@ -38,6 +44,8 @@ const Slider = () => {
               </div>
             </div>
           </div>
+
+          {/* ----Pagination----- */}
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
